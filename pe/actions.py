8bf98618bb90874a2d874fe89d202f36d7cd25e5
Michael Wayne Goodman <goodman.m.w@gmail.com>
2020-04-23 10:15:04 +0800

    Improve repr of Action subclasses

--- pe/actions.py
+++ pe/actions.py
@@ -6,6 +6,9 @@ from pe._match import determine
 
 
 class Action:
+    def __repr__(self):
+        return f'{type(self).__name__}({self.arg!r})'
+
     def __call__(self,
                  s: str,
                  pos: int,
@@ -17,71 +20,74 @@ class Action:
 
 class Call(Action):
     def __init__(self, func):
-        self.func = func
+        self.arg = func
 
     def __call__(self, s, pos, end, args, kwargs):
-        return (self.func(*args, **kwargs),), None
+        return (self.arg(*args, **kwargs),), None
 
 
 class Raw(Action):
     def __init__(self, func=str):
-        self.func = func
+        self.arg = func
 
     def __call__(self, s, pos, end, args, kwargs):
-        return (self.func(s[pos:end]),), None
+        return (self.arg(s[pos:end]),), None
 
 
 class Bind(Action):
     def __init__(self, name: str):
-        self.name = name
+        self.arg = name
 
     def __call__(self, s, pos, end, args, kwargs):
         kwargs = dict(kwargs or [])
-        kwargs[self.name] = determine(args)
+        kwargs[self.arg] = determine(args)
         return (), kwargs
 
 
 class Constant(Action):
 
     def __init__(self, value):
-        self.value = value
+        self.arg = value
 
     def __call__(self, s, pos, end, args, kwargs):
-        return (self.value,), None
+        return (self.arg,), None
 
 
 class Pack(Action):
 
     def __init__(self, func):
-        self.func = func
+        self.arg = func
 
     def __call__(self, s, pos, end, args, kwargs):
-        return (self.func(args, **kwargs),), None
+        return (self.arg(args, **kwargs),), None
 
 
 class Join(Action):
 
     def __init__(self, func, sep=''):
-        self.func = func
+        self.arg = func
         self.sep = sep
 
+    def __repr__(self):
+        return f'{type(self).__name__}({self.arg!r}, {self.sep!r})'
+
     def __call__(self, s, pos, end, args, kwargs):
-        return (self.func(self.sep.join(args), **kwargs),), None
+        return (self.arg(self.sep.join(args), **kwargs),), None
 
 
 class Getter(Action):
     def __init__(self, i):
-        self.i = i
+        self.arg = i
 
     def __call__(self, s, pos, end, args, kwargs):
-        return (args[self.i],), None
+        return (args[self.arg],), None
 
 
 class Fail(Action):
 
     def __init__(self, message):
-        self.message = message
+        self.arg = message
 
     def __call__(self, s, pos, end, args, kwargs):
         raise ParseError.from_pos(
-            pos, s, message=self.message.format(*args, **kwargs))
+            pos, s, message=self.arg.format(*args, **kwargs))
