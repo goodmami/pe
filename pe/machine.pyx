6f9a40def1f69694ee6b7c9725d4c8cec123bedb
Michael Wayne Goodman <goodman.m.w@gmail.com>
2021-05-22 13:19:47 +0800

    Move Cython scanners to separate module

--- pe/machine.pyx
+++ pe/machine.pyx
@@ -18,8 +18,8 @@ from pe._match import Match
 from pe._types import Memo
 from pe._grammar import Grammar
 from pe._parser import Parser
-from pe._escape import unescape
 from pe._optimize import optimize
+from pe.scanners cimport Scanner, Dot, Literal, CharacterClass, Regex
 from pe.actions import Action, Bind
 from pe.operators import Rule
 
@@ -94,7 +94,8 @@ cdef class Instruction:
 
     def __init__(
         self,
-        OpCode opcode, short oploc=1,
+        OpCode opcode,
+        short oploc=1,
         Scanner scanner=None,
         bint marking=False,
         bint capturing=False,
@@ -351,8 +352,8 @@ def _cls(defn, mincount=1, maxcount=1):
 
 
 def _rgx(defn):
-    pat = re.compile(f'{defn.args[0]}', flags=defn.args[1])
-    return [Instruction(SCAN, scanner=Regex(pat))]
+    pat, flags = defn.args
+    return [Instruction(SCAN, scanner=Regex(pat, flags=flags))]
 
 
 def _opt(defn):
@@ -437,7 +438,6 @@ def _seq(defn):
 
 
 def _chc(defn):
-    # TODO: action
     pis = [_parsing_instructions(d) for d in defn.args[0]]
     pi = pis[-1]
     for _pi in reversed(pis[:-1]):
@@ -490,104 +490,3 @@ def _parsing_instructions(defn):  # noqa: C901
         return _op_map[defn.op](defn)
     except KeyError:
         raise Error(f'invalid definition: {defn!r}')
-
-
-cdef class Scanner:
-    cpdef int scan(self, str s, int pos=0) except -2:
-        try:
-            return self._scan(s, pos, len(s))
-        except IndexError:
-            return FAILURE
-
-    cdef int _scan(self, str s, int pos, int slen) except -2:
-        return FAILURE
-
-
-cdef class Dot(Scanner):
-    cdef int _scan(self, str s, int pos, int slen) except -2:
-        if pos < slen:
-            return pos + 1
-        return FAILURE
-
-
-cdef class Literal(Scanner):
-    cdef str _x
-    cdef int _xlen
-
-    def __init__(self, str x):
-        self._x = x
-        self._xlen = len(x)
-
-    cdef int _scan(self, str s, int pos, int slen) except -2:
-        cdef int end = pos + self._xlen
-        if s[pos:end] != self._x:
-            return FAILURE
-        return end
-
-
-cdef class CharacterClass(Scanner):
-    cdef str _chars
-    cdef str _ranges
-    cdef int _rangelen
-    cdef bint _negate
-    cdef public:
-        int mincount, maxcount
-
-    def __init__(
-        self,
-        list ranges,
-        bint negate = False,
-        int mincount = 1,
-        int maxcount = 1
-    ):
-        self._chars = ''.join(a for a, b in ranges if not b)
-        self._ranges = ''.join(a+b for a, b in ranges if b)
-        self._rangelen = len(self._ranges)
-        self._negate = negate
-        self.mincount = mincount
-        self.maxcount = maxcount
-
-    cdef int _scan(self, str s, int pos, int slen) except -2:
-        cdef Py_UCS4 c
-        cdef str ranges = self._ranges
-        cdef bint matched
-        cdef int mincount = self.mincount
-        cdef int maxcount = self.maxcount
-        cdef int i = 0
-        while maxcount and pos < slen:
-            c = s[pos]
-            matched = False
-            if c in self._chars:
-                matched = True
-            else:
-                while i < self._rangelen:
-                    if ranges[i] <= c <= ranges[i+1]:
-                        matched = True
-                        break
-                    i += 2
-            if matched ^ self._negate:
-                pos += 1
-                maxcount -= 1
-                mincount -= 1
-            else:
-                break
-        if mincount > 0:
-            return FAILURE
-        return pos
-
-
-cdef class Regex(Scanner):
-    cdef object _regex
-
-    def __init__(self, object pattern):
-        if hasattr(pattern, 'match'):
-            self._regex = pattern
-        else:
-            self._regex = re.compile(pattern)
-
-    cdef int _scan(self, str s, int pos, int slen) except -2:
-        m = self._regex.match(s, pos=pos)
-        if m is None:
-            return FAILURE
-        else:
-            return m.end()
