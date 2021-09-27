3fcebee29477a11432e682d62b8c975a1ec82291
Michael Wayne Goodman <goodman.m.w@gmail.com>
2021-09-27 16:52:28 -0700

    Move mostly to static build config; improve sdist
    The sdist now includes the *.c, *.pyx, and *.pxd files. Also, Cython
    was removed as a build requirement, which means the sdist can be
    installed with the pure-Python sources and won't try to build the
    extension modules. In the future it might make sense to build the
    extension by default.

--- setup.py
+++ setup.py
@@ -1,50 +1,31 @@
 #!/usr/bin/env python3
 
+from typing import Dict
+import sys
 import os
 
-from setuptools import setup
-from Cython.Build import cythonize
+from setuptools import setup, Extension
+
+# Conditional Cython setup
+
+try:
+    from Cython.Build import cythonize
+    extensions = cythonize('pe/*.pyx', language_level='3')
+except ImportError:
+    extensions = []  # don't build C files for source install
+
+
+# Dynamic project metadata
 
 base_dir = os.path.dirname(__file__)
-
-long_description = ''
-with open(os.path.join(base_dir, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-about = {}
+meta: Dict[str, str] = {}
 with open(os.path.join(base_dir, "pe", "_meta.py")) as f:
-    exec(f.read(), about)
+    exec(f.read(), meta)
 
 setup(
-    name=about['__title__'],
-    version=about['__version__'],
-    description=about['__summary__'],
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url=about['__uri__'],
-    author=about['__author__'],
-    author_email=about['__email__'],
-    license=about['__license__'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        "Programming Language :: Cython",
-        "Programming Language :: Python",
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Processing',
-        'Topic :: Utilities'
-    ],
-    keywords='peg parsing text',
-    packages=[
-        'pe',
-    ],
-    ext_modules=cythonize('pe/*.pyx', language_level='3'),
-    python_requires='>=3.6'
+    version=meta['__version__'],
+    ext_modules=extensions,
+    package_data={
+        'pe': ['*.pyx', '*.pxd'],
+    }
 )
