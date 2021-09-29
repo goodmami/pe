74ee5fef27adab6e70106916404676ff9210c952
Michael Wayne Goodman <goodman.m.w@gmail.com>
2021-09-29 15:58:46 -0700

    Only use Cy MachineParser on CPython if available

--- pe/machine.py
+++ pe/machine.py
@@ -1,8 +1,21 @@
 
-try:
-    from pe._cy_machine import MachineParser
-except ImportError:
-    from pe._py_machine import MachineParser  # type: ignore
+import sys
 
+from typing import Type
+from pe._parser import Parser
+from pe import _py_machine
+try:
+    from pe import _cy_machine
+except ImportError:
+    _cy_machine = None  # type: ignore
 
 __all__ = ['MachineParser']
+
+
+MachineParser: Type[Parser]
+
+# only use the extension module if it's available and we're in CPython
+if _cy_machine is None or sys.implementation.name != 'cpython':
+    MachineParser = _py_machine.MachineParser
+else:
+    MachineParser = _cy_machine.MachineParser
